a09d031f34950259fcb03fdf9f7bce4d84731791
vincentBesseau <33568805+vincentBesseau@users.noreply.github.com>
2019-05-29 13:41:29 +0200

    Ajout de la methode retournant une piece

--- server/src/core/room/index.ts
+++ server/src/core/room/index.ts
@@ -2,6 +2,7 @@ import create from './room.create';
 import update from './room.update';
 import destroy from './room.destroy';
 import get from './room.get';
+import getRoom from './room.getRoom';
 import getDevices from './room.getDevices';
 import getSatellites from './room.getSatellites';
 import getState from './room.getState';
@@ -11,6 +12,7 @@ export default class Room {
     update = update;
     destoy = destroy;
     get = get;
+    getRoom = getRoom;
     getDevice = getDevices;
     getSatellites = getSatellites;
     getState = getState;
