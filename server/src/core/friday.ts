926188506b5c3d36f9a3c0d28d0d25a184402344
vincentBesseau <vbesseau44@gmail.com>
2019-05-30 22:30:29 +0200

    Ajout du core satellite

--- server/src/core/friday.ts
+++ server/src/core/friday.ts
@@ -1,11 +1,13 @@
 import House from './house';
 import Room from './room';
 import User from './user';
+import Satellite from './satellite';
 import * as Constants from '../utils/constants';
 
 export {
     House,
     Room,
     User,
+    Satellite,
     Constants
 };
