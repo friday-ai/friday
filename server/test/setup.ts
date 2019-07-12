4c994e62c7c9c1339f7ac2eb26b45da939d1c90d
Mathieu Andrade <26581253+MathieuAndrade@users.noreply.github.com>
2019-07-12 16:19:09 +0200

    Increase test timeout

--- server/test/setup.ts
+++ server/test/setup.ts
@@ -2,6 +2,7 @@ import * as database from '../src/config/database';
 import { seedDb, cleanDb } from './utils/seed';
 
 beforeAll(async () => {
+  jest.setTimeout(7000);
   await database.init();
   await seedDb();
 });
