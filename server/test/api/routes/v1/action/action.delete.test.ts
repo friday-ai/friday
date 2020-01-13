4e8020a4401ad6d12e95d819fa0597b5c9de9ea6
vincentBesseau <vbesseau44@gmail.com>
2020-01-13 22:26:45 +0100

    Fix ano on action API DELETE test

--- server/test/api/routes/v1/action/action.delete.test.ts
+++ server/test/api/routes/v1/action/action.delete.test.ts
@@ -8,7 +8,7 @@ describe('action.delete', () => {
       .delete('/api/v1/action/33ab56b0-4064-40d0-b1f4-1e426bff1ea3')
       .expect(200)
       .then((res) => {
-        expect(res.body).toEqual(true);
+        expect(res.body.success).toEqual(true);
       });
   });
 
