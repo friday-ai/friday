e4c6a56d6cb6e89b9271e7d0f83aed552dd10d62
vincentBesseau <vbesseau44@gmail.com>
2020-01-13 22:23:08 +0100

    Add device API tests for CRUD

---
+++ server/test/api/routes/v1/device/device.getAll.test.ts
@@ -0,0 +1,169 @@
+import TestServer from "../../../../utils/helper";
+import DeviceType from "../../../../../src/core/device/device.interface";
+
+describe('action.getAll', () => {
+
+  it('should return all devices', async () => {
+
+    const server = await new TestServer();
+
+    await server
+      .get('/api/v1/device')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual([
+          {
+            id: '22b5b9ce-cd9e-404a-8c31-97350d684fd3',
+            name: 'Light',
+            type: 'light',
+            subType: 'light_rgb',
+            variable: '',
+            unit: '',
+            value: 'on',
+            roomId: 'c97ba085-ba97-4a30-bdd3-b7a62f6514dc',
+            pluginId: '33ddf1e2-3c51-4426-93af-3b0453ac0c1e'
+          },
+          {
+            id: 'cc306435-eb0f-455c-b79d-a684b171e04d',
+            name: 'Temperature',
+            type: 'sensor',
+            subType: 'sensor_temperature',
+            variable: '',
+            unit: '°C',
+            value: '23',
+            roomId: 'c97ba085-ba97-4a30-bdd3-b7a62f6514dc',
+            pluginId: '88b48273-15e6-4729-9199-0682677475f4'
+          }
+        ]);
+      });
+
+  });
+
+  it('should return all devices with full scope', async () => {
+
+    const server = await new TestServer();
+
+    await server
+      .get('/api/v1/device')
+      .query({'scope' : 'full'})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        let devices = res.body;
+        expect(devices).toBeArray();
+        devices.forEach((device: DeviceType) => {
+          expect(device).toBeObject();
+          expect(device).toContainAllKeys(
+            ["id", "name", "type", "subType", "variable", "unit", "value", "roomId", "pluginId", "room", "plugin", "state"]
+          );
+          if(device.state !== null) {
+            expect(device.state).toBeObject();
+            expect(device.state).toHaveProperty('id');
+            expect(device.state).toHaveProperty('owner');
+            expect(device.state).toHaveProperty('ownerType');
+            expect(device.state).toHaveProperty('value');
+          }
+
+          expect(device.room).toBeObject();
+          expect(device.room).toHaveProperty('id');
+          expect(device.room).toHaveProperty('name');
+          expect(device.room).toHaveProperty('houseId');
+
+          if (device.plugin !== null) {
+            expect(device.plugin).toBeObject();
+            expect(device.plugin).toHaveProperty('id');
+            expect(device.plugin).toHaveProperty('name');
+            expect(device.plugin).toHaveProperty('version');
+            expect(device.plugin).toHaveProperty('url');
+            expect(device.plugin).toHaveProperty('enabled');
+            expect(device.plugin).toHaveProperty('satelliteId');
+          }
+        })
+    });
+  });
+
+  it('should return all devices with state', async () => {
+    const server = await new TestServer();
+
+    await server
+      .get('/api/v1/device')
+      .query({'scope' : 'withState'})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        let devices = res.body;
+        expect(devices).toBeArray();
+        devices.forEach((device: DeviceType) => {
+          expect(device).toBeObject();
+          expect(device).toContainAllKeys(
+            ["id", "name", "type", "subType", "variable", "unit", "value", "roomId", "pluginId", "state"]
+          );
+          if(device.state !== null) {
+            expect(device.state).toBeObject();
+            expect(device.state).toHaveProperty('id');
+            expect(device.state).toHaveProperty('owner');
+            expect(device.state).toHaveProperty('ownerType');
+            expect(device.state).toHaveProperty('value');
+          }
+        })
+      });
+  });
+
+  it('should return all Devices with room', async () => {
+    const server = await new TestServer();
+
+    await server
+      .get('/api/v1/device')
+      .query({'scope' : 'withRoom'})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        let devices = res.body;
+        expect(devices).toBeArray();
+        devices.forEach((device: DeviceType) => {
+          expect(device).toBeObject();
+          expect(device).toContainAllKeys(
+            ["id", "name", "type", "subType", "variable", "unit", "value", "roomId", "pluginId", "room"]
+          );
+
+          expect(device.room).toBeObject();
+          expect(device.room).toHaveProperty('id');
+          expect(device.room).toHaveProperty('name');
+          expect(device.room).toHaveProperty('houseId');
+        })
+      });
+  });
+
+  it('should return all Devices with plugin', async () => {
+
+    const server = await new TestServer();
+
+    await server
+      .get('/api/v1/device')
+      .query({'scope' : 'withPlugin'})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        let devices = res.body;
+        expect(devices).toBeArray();
+        devices.forEach((device: DeviceType) => {
+          expect(device).toBeObject();
+          expect(device).toContainAllKeys(
+            ["id", "name", "type", "subType", "variable", "unit", "value", "roomId", "pluginId", "plugin"]
+          );
+
+          if (device.plugin !== null) {
+            expect(device.plugin).toBeObject();
+            expect(device.plugin).toHaveProperty('id');
+            expect(device.plugin).toHaveProperty('name');
+            expect(device.plugin).toHaveProperty('version');
+            expect(device.plugin).toHaveProperty('url');
+            expect(device.plugin).toHaveProperty('enabled');
+            expect(device.plugin).toHaveProperty('satelliteId');
+          }
+        })
+      });
+  });
+
+});
---
+++ server/test/api/routes/v1/device/device.update.test.ts
@@ -0,0 +1,31 @@
+import TestServer from "../../../../utils/helper";
+
+describe('device.update', () => {
+  it('should update a device', async () => {
+    const server = await new TestServer();
+
+    await server
+      .patch('/api/v1/device/22b5b9ce-cd9e-404a-8c31-97350d684fd3')
+      .send({
+        name: 'Device update'
+      })
+      .expect(200)
+      .then((res) => {
+        let body = res.body;
+        expect(body).toBeObject();
+        expect(body.name).toEqual('Device update');
+      })
+  });
+
+  it('should not found device to update', async () => {
+    const server = await new TestServer();
+
+    await server
+      .patch('/api/v1/device/449b2033-105f-4c18-91e8-a56ad1831796')
+      .send({
+        name: 'Action update'
+      })
+      .expect(404);
+
+  });
+});
