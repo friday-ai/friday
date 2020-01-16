740ba694318499de1542550ecba6f636183d2ce6
vincentBesseau <vbesseau44@gmail.com>
2020-01-16 22:48:11 +0100

    Add satellite API tests for CRUD

--- server/src/core/satellite/satellite.update.ts
+++ server/src/core/satellite/satellite.update.ts
@@ -4,19 +4,22 @@ import { default as error, NotFoundError} from '../../utils/errors/coreError';
 
 /**
  * Update a satellite.
+ * @param {String} id - Id of satellite
  * @param {SatelliteType} satellite - A satellite object.
  * @returns {Promise<SatelliteType>} Resolve with updated satellite.
  * @example
  * ````
- * friday.satellite.update({
+ * friday.satellite.update(
+ * '31f61b90-27cc-4bf6-9855-4cc59526157b',
+ * {
  *   id: '31f61b90-27cc-4bf6-9855-4cc59526157b'
  *   name: 'satellite update'
  * });
  * ````
  */
-export default async function update(satellite: SatelliteType): Promise<SatelliteType> {
+export default async function update(id: string, satellite: SatelliteType): Promise<SatelliteType> {
   try {
-    const satelliteToUpdate = await Satellite.findByPk(satellite.id);
+    const satelliteToUpdate = await Satellite.findByPk(id);
 
     if (satelliteToUpdate === null) {
       throw new NotFoundError({name: 'Update a Stallite', message: 'Satellite not found', metadata: satellite.id});
---
+++ server/test/api/routes/v1/satellite/satellite.delete.test.ts
@@ -0,0 +1,23 @@
+import TestServer from "../../../../utils/helper";
+
+describe('satellite.delete', () => {
+  it('should delete a satellite', async () => {
+    const server = await new TestServer();
+
+    await server
+      .delete('/api/v1/satellite/a7ef5f08-2bad-4489-95bf-b73fcf894d8f')
+      .expect(200)
+      .then((res) => {
+        expect(res.body.success).toEqual(true);
+      });
+  });
+
+  it('should not found satellite to delete', async () => {
+    const server = await new TestServer();
+
+    await server
+      .delete('/api/v1/satellite/a7ef5f08-2bad-4489-95bf-b73fcf894333')
+      .expect(404);
+  });
+
+});
