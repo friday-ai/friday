ac34fef2aca6d3d326f39f423cd9047d8dbf9207
vincentBesseau <vbesseau44@gmail.com>
2020-01-17 23:52:35 +0100

    Add script API tests for CRUD

--- server/test/api/routes/v1/script/script.delete.test.ts
+++ server/test/api/routes/v1/script/script.delete.test.ts
@@ -1,22 +1,22 @@
 import TestServer from "../../../../utils/helper";
 
-describe('scene.delete', () => {
-  it('should delete a scene', async () => {
+describe('script.delete', () => {
+  it('should delete a script', async () => {
     const server = await new TestServer();
 
     await server
-      .delete('/api/v1/scene/2452964a-a225-47dd-9b83-d88d57ed280e')
+      .delete('/api/v1/script/d354cede-3895-4dac-8a90-73d970b4617c')
       .expect(200)
       .then((res) => {
         expect(res.body.success).toEqual(true);
       });
   });
 
-  it('should not found scene to delete', async () => {
+  it('should not found satellite to delete', async () => {
     const server = await new TestServer();
 
     await server
-      .delete('/api/v1/scene/2452964a-a225-47dd-9b83-d88d57ed2333')
+      .delete('/api/v1/script/d354cede-3895-4dac-8a90-73d970b46333')
       .expect(404);
   });
 
---
+++ server/test/api/routes/v1/script/script.getAll.test.ts
@@ -0,0 +1,23 @@
+import TestServer from "../../../../utils/helper";
+
+describe('scipt.getAll', () => {
+
+  it('should return all script', async () => {
+
+    const server = await new TestServer();
+
+    await server
+      .get('/api/v1/script')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual([
+          {
+            id: 'd354cede-3895-4dac-8a90-73d970b4617c',
+            name: 'Test Script',
+            code: 'console.log(\'Hey ! This script is a test ! :)\')'
+          }
+        ]);
+      });
+  });
+});
