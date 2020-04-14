2a3f58a2392be9525ab15512352899ec9f665679
Mathieu Andrade <mathieu.botelhoandrade@gmail.com>
2020-04-14 11:09:54 +0200

    Rewrite action tests

--- server/test/core/action/action.create.test.ts
+++ server/test/core/action/action.create.test.ts
@@ -1,8 +1,9 @@
+import { expect, assert } from 'chai';
 import Action from '../../../src/core/action';
 import { ActionsType } from '../../../src/utils/constants';
 import { DatabaseUniqueConstraintError, DatabaseValidationError } from '../../../src/utils/errors/coreError';
 
-describe('action.create', () => {
+describe('Action.create', () => {
   const action = new Action();
 
   it('should create a action', async () => {
@@ -17,20 +18,19 @@ describe('action.create', () => {
       sceneId: '2452964a-a225-47dd-9b83-d88d57ed280e'
     });
 
-    expect(createdAction).toHaveProperty('id');
-    expect(createdAction).toHaveProperty('name');
-    expect(createdAction).toHaveProperty('description');
-    expect(createdAction).toHaveProperty('type');
-    expect(createdAction).toHaveProperty('subType');
-    expect(createdAction).toHaveProperty('variableKey');
-    expect(createdAction).toHaveProperty('variableValue');
-    expect(createdAction).toHaveProperty('sceneId');
+    expect(createdAction).to.have.property('id');
+    expect(createdAction).to.have.property('name');
+    expect(createdAction).to.have.property('description');
+    expect(createdAction).to.have.property('type');
+    expect(createdAction).to.have.property('subType');
+    expect(createdAction).to.have.property('variableKey');
+    expect(createdAction).to.have.property('variableValue');
+    expect(createdAction).to.have.property('sceneId');
   });
 
   it('should not create a action with an existing name', async () => {
-    expect.assertions(1);
 
-    await action.create({
+    const promise = action.create({
       id: '70721298-9668-4fdf-b09c-7b8ddf6ff4d9',
       name: 'action1',
       description: 'action test description',
@@ -39,16 +39,14 @@ describe('action.create', () => {
       variableKey: 'action test variable key',
       variableValue: 'action test variable value',
       sceneId: '2452964a-a225-47dd-9b83-d88d57ed280e'
-    })
-      .catch((err: Error) => {
-        expect(err).toBeInstanceOf(DatabaseUniqueConstraintError);
-      });
+    });
+
+    await assert.isRejected(promise, DatabaseUniqueConstraintError);
   });
 
   it('should not create a action with an empty name', async () => {
-    expect.assertions(1);
 
-    await action.create({
+    const promise = action.create({
       id: 'bd362b3b-21f0-4121-a967-4071b9a837c0',
       name: '',
       description: 'action test description',
@@ -57,16 +55,14 @@ describe('action.create', () => {
       variableKey: 'action test variable key',
       variableValue: 'action test variable value',
       sceneId: '2452964a-a225-47dd-9b83-d88d57ed280e'
-    })
-      .catch((err: Error) => {
-        expect(err).toBeInstanceOf(DatabaseValidationError);
-      });
+    });
+
+    await assert.isRejected(promise, DatabaseValidationError);
   });
 
   it('should not create a action with a empty scene id', async () => {
-    expect.assertions(1);
 
-    await action.create({
+    const promise = action.create({
       id: 'd175c84f-a5fc-4afb-beeb-5cd57a033844',
       name: 'action2',
       description: 'action test description',
@@ -75,16 +71,14 @@ describe('action.create', () => {
       variableKey: 'action test variable key',
       variableValue: 'action test variable value',
       sceneId: ''
-    })
-      .catch((err: Error) => {
-        expect(err).toBeInstanceOf(DatabaseValidationError);
-      });
+    });
+
+    await assert.isRejected(promise, DatabaseValidationError);
   });
 
   it('should not create a action with a wrong scene id', async () => {
-    expect.assertions(1);
 
-    await action.create({
+    const promise = action.create({
       id: 'c79e34cc-8bbf-4134-89c0-b33c8e8761e5',
       name: 'action3',
       description: 'action test description',
@@ -93,10 +87,9 @@ describe('action.create', () => {
       variableKey: 'action test variable key',
       variableValue: 'action test variable value',
       sceneId: '99a2e8fd-2298-40e4-93d6-9866c3261ec0'
-    })
-      .catch((err: Error) => {
-        expect(err).toBeInstanceOf(DatabaseValidationError);
-      });
+    });
+
+    await assert.isRejected(promise, DatabaseValidationError);
   });
 
 });
--- server/test/core/action/action.destroy.test.ts
+++ server/test/core/action/action.destroy.test.ts
@@ -1,20 +1,17 @@
+import { assert } from 'chai';
 import Action from '../../../src/core/action';
 import { NotFoundError } from '../../../src/utils/errors/coreError';
 
-describe('action.destoy', () => {
+describe('Action.destoy', () => {
   const action = new Action();
 
   it('should destroy an action', async () => {
-    expect.assertions(0);
     await action.destroy('33ab56b0-4064-40d0-b1f4-1e426bff1ea3');
   });
 
   it('should not found an action to destroy', async () => {
-    expect.assertions(1);
-    await action.destroy('a58c31cc-61d2-4c18-b9f6-b8ba8609d12e')
-    .catch((err: Error) => {
-      expect(err).toBeInstanceOf(NotFoundError);
-    });
+    const promise = action.destroy('a58c31cc-61d2-4c18-b9f6-b8ba8609d12e');
+    await assert.isRejected(promise, NotFoundError);
   });
 
 });
