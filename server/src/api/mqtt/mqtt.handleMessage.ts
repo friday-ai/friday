580806be74fb6082601b974b3adc962a9df4988c
vincentBesseau <vbesseau44@gmail.com>
2020-10-14 22:41:37 +0200

    Adding all subscribing topics to communicate with master (#20)

--- server/src/api/mqtt/mqtt.handleMessage.ts
+++ server/src/api/mqtt/mqtt.handleMessage.ts
@@ -1,5 +1,6 @@
 import MqttServer from '.';
 import Log from '../../utils/log';
+import friday from '../../core/friday';
 
 const logger = new Log();
 
@@ -8,8 +9,26 @@ export default function handleMessage(this: MqttServer, topic: string, message: 
   logger.info(`Receive message on topic ${topic} (${message})`);
   switch (topic) {
     // @Todo Add topic
-    case '':
-
+    case 'friday/master/satelitte/heartbeat':
+      // this.friday.satellite
+      break;
+    case 'friday/master/satelitte/discoverme':
+      this.friday.satellite.create(json);
+      break;
+    case 'friday/master/plugin/heartbeat':
+      // this.friday.plugin
+      break;
+    case 'friday/master/plugin/discoverme':
+      this.friday.plugin.create(json);
+      break;
+    case 'friday/master/device/set':
+      this.friday.device.create(json);
+      break;
+    case 'friday/master/device/delete':
+      this.friday.device.destroy(json.id);
+      break;
+    case 'friday/master/state/set':
+      this.friday.state.set(json);
       break;
     default:
       logger.error(`Friday is not subscribed to this topic ${topic}`);
