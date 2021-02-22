23099a55fa7c2ebd5201c07c68da9880799311e7
Mathieu Andrade <mathieu.botelhoandrade@gmail.com>
2021-02-22 11:56:16 +0100

    Fix Plugin install test

--- server/test/core/plugin/plugin.install.test.ts
+++ server/test/core/plugin/plugin.install.test.ts
@@ -5,12 +5,12 @@ import Variable from '../../../src/core/variable';
 import State from '../../../src/core/state';
 import Docker from '../../../src/core/docker';
 
-describe('Plugin.update', () => {
+describe('Plugin.install', () => {
   const event = new Event();
   const variable = new Variable();
   const state = new State(event, variable);
   const docker = new Docker();
-  const plugin = new Plugin('e2cz8cc-60a7-4c40-87d2-b25048b1aa04', docker, state);
+  const plugin = new Plugin('a7ef5f08-2bad-4489-95bf-b73fcf894d8f', docker, state);
 
   it('should install a plugin', async () => {
     const installedPlugin = await plugin.install({
