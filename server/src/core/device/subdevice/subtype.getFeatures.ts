40e724b6ac5f5e6423535fc6696780b7c00a451b
vincentBesseau <vbesseau44@gmail.com>
2021-04-17 11:53:07 +0200

    Adding try catch on method #18

--- server/src/core/device/subdevice/subtype.getFeatures.ts
+++ server/src/core/device/subdevice/subtype.getFeatures.ts
@@ -1,4 +1,5 @@
 import { DEVICE_TYPE_COMMON_FEATURE, DEVICE_SUBTYPE_FEATURE_LIST, DEVICE_SUBTYPE_LIST } from '../../../utils/device.constants';
+import error from '../../../utils/errors/coreError';
 
 /**
  * Récupère la liste des features par rapport a un device et a un subdevice donnée
@@ -9,17 +10,23 @@ import { DEVICE_TYPE_COMMON_FEATURE, DEVICE_SUBTYPE_FEATURE_LIST, DEVICE_SUBTYPE
  * @return {[key: string]: string}
  */
 const getFeatures = (device: string, subdevice: string): { [key: string]: string; } => {
-  if (!(device in DEVICE_SUBTYPE_LIST)) {
-    throw new Error(`${device} ne fais pas partie des devices présent`);
-  }
+  try {
+    if (!(device in DEVICE_SUBTYPE_LIST)) {
+      throw new Error(`${device} ne fais pas partie des devices présent`);
+    }
 
-  if (!(subdevice in DEVICE_SUBTYPE_LIST[device])) {
-    throw new Error(`${subdevice} ne fais pas partie des subdevices présent dans le device ${device}`);
-  }
+    if (!(subdevice in DEVICE_SUBTYPE_LIST[device])) {
+      throw new Error(`${subdevice} ne fais pas partie des subdevices présent dans le device ${device}`);
+    }
 
-  const deviceFeature = DEVICE_TYPE_COMMON_FEATURE[device];
-  const subdeviceFeature = DEVICE_SUBTYPE_FEATURE_LIST[device][subdevice];
-  return { ...deviceFeature, ...subdeviceFeature };
+    const deviceFeature = DEVICE_TYPE_COMMON_FEATURE[device];
+    const subdeviceFeature = DEVICE_SUBTYPE_FEATURE_LIST[device][subdevice];
+    return { ...deviceFeature, ...subdeviceFeature };
+  } catch (e) {
+    throw error({
+      name: e.name, message: e.message, cause: e, metadata: { device, subdevice },
+    });
+  }
 };
 
 export default {
