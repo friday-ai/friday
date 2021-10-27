6d5a3c2ee55be078ed3a58cadf7556c1a6097989
Mathieu Andrade <mathieu.botelhoandrade@gmail.com>
2021-10-27 14:29:25 +0200

    Add new keys in Tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -14,6 +14,15 @@ module.exports = {
     ],
   },
   theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+      'max-2xl': {'max': '1535px'},
+      'max-xl': {'max': '1279px'},
+      'max-lg': {'max': '1023px'},
+      'max-md': {'max': '767px'},
+      'max-sm': {'max': '639px'},
+    },
     extend: {
       colors: {
         "blue": {
@@ -59,6 +68,28 @@ module.exports = {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
+      gridTemplateColumns: {
+        'minmax-280': 'repeat(auto-fit, minmax(278px, 280px))',
+      },
+      animation: {
+        enter: 'enter 200ms ease-out',
+        leave: 'leave 150ms ease-in forwards',
+        'slide-in': 'slide-in 1.2s cubic-bezier(.41,.73,.51,1.02)',
+      },
+      keyframes: {
+        enter: {
+          '0%': { transform: 'scale(0.9)', opacity: 0 },
+          '100%': { transform: 'scale(1)', opacity: 1 },
+        },
+        leave: {
+          '0%': { transform: 'scale(1)', opacity: 1 },
+          '100%': { transform: 'scale(0.9)', opacity: 0 },
+        },
+        'slide-in': {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+      }
     },
   },
   variants: {},
