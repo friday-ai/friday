7872e2ef3deb40786336335f31d0ce87baef82b5
Mathieu Andrade <mathieu.botelhoandrade@gmail.com>
2022-08-22 20:40:06 +0200

    Add Countdown component

---
+++ client/src/components/Countdown/Countdown.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+
+interface CountdownProps {
+  start: boolean;
+  count: number;
+}
+
+const Countdown: React.FC<CountdownProps> = ({ start, count }) => {
+  useEffect(() => {
+    if (start) {
+      let counter = count;
+      const style = document.getElementById('countdown')?.style;
+      style?.setProperty('--value', counter.toString());
+      setInterval(() => {
+        if (counter > 0) {
+          counter -= 1;
+          style?.setProperty('--value', counter.toString());
+        }
+      }, 1000);
+    }
+  }, [start, count]);
+
+  return (
+    <span className="countdown">
+      <span id="countdown" />
+    </span>
+  );
+};
+
+export default Countdown;
