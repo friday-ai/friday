4ae90029cfafbf980092899cea4f65c4f6cb14f0
Mathieu Andrade <Mathieu.botelhoandrade@gmail.com>
2023-08-21 13:39:36 +0200

    Front: Add Countdown component

---
+++ apps/front/src/components/Countdown/Countdown.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+
+interface CountdownProps {
+  start: boolean;
+  count: number;
+}
+
+export default function Countdown({ start, count }: CountdownProps) {
+  useEffect(() => {
+    if (start) {
+      let counter = count;
+      const style = document.getElementById('countdown')?.style;
+      style?.setProperty('--value', counter.toString());
+      setInterval(() => {
+        if (counter > 0) {
+          counter -= 1;
+          style?.setProperty('--value', counter.toString());
+        }
+      }, 1000);
+    }
+  }, [start, count]);
+
+  return (
+    <span className="countdown">
+      <span id="countdown" />
+    </span>
+  );
+}
