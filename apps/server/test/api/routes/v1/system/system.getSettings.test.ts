cce9a010fd726fbbe3a3f78cbe3afcd4e37d10b0
Mathieu Andrade <Mathieu.botelhoandrade@gmail.com>
2024-01-03 00:05:19 +0100

    Back: Add test for new function "getSettings"

---
+++ apps/server/test/api/routes/v1/system/system.getSettings.test.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import server from '../../../../utils/request';
+
+describe('GET /api/v1/system/settings', () => {
+  it('should get settings of friday', async () => {
+    await server
+      .get('/api/v1/system/settings')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).to.contains.keys(['version', 'units', 'history']);
+      });
+  });
+});
