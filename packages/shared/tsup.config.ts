d7f20b1c8ae49d07ab444ed78a20dd116710b072
Mathieu Andrade <mathieu.botelhoandrade@gmail.com>
2024-03-24 15:07:17 +0100

    Big project update
    Complete refractor of shared package:
    - Use Tsup to compile in single file
    - Clean useless dependencies

---
+++ packages/shared/tsup.config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+import type { Options } from 'tsup';
+
+export const tsup: Options = {
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  minify: true,
+  clean: true,
+  dts: true,
+  skipNodeModulesBundle: false,
+  format: ['cjs', 'esm'],
+  outDir: 'lib',
+};
